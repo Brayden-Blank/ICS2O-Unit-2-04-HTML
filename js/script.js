e712f0428df068ce0c67ea5869a15dc9d8886d71
Brayden-Blank <99354754+Brayden-Blank@users.noreply.github.com>
2022-03-28 12:51:41 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Created by: Brayden Blank
+// Created on: March 2022
+// This file contains the JS functions for index.html
+
+'use-strict'
+/**
+ * This function calculates area of a triangle.
+ */
+function calculate () {
+  //input
+  const base = parseInt(document.getElementById('base').value)
+  const height = parseInt(document.getElementById('height').value)
+
+  //process
+  const area = base * height
+
+  //output
+  document.getElementById('area').innerHTML = 'The area is: ' + area + ' cm²!'
+}
